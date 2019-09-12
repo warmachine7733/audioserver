26ae0e9568bf0f9985562ea40b1223b482290d82
prateek.jena <prateek.jena@sparity.com>
2019-09-12 18:14:18 +0530

    added get by id api

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 const Audio = require("../models/audio");
 const { getAudioDurationInSeconds } = require("get-audio-duration");
 const NodeID3 = require("node-id3");
+const mongoose = require("mongoose");
 
+const ObjectId = require("mongodb").ObjectID;
 const Joi = require("joi");
 
 //promise
@@ -25,7 +27,7 @@ module.exports = {
         url: req.file.filename,
         title: req.body.title,
         cover: "default.jpg",
-        duration: duration,
+        duration: Math.floor(duration),
         artist: req.body.artist
       };
       let newBike = new Audio(tempAudio);
@@ -47,9 +49,13 @@ module.exports = {
   },
   getSelectedAudio: async (req, res, next) => {
     try {
-      const id = req.body.id;
-      const result = await Audio.find({ id });
-      console.log("result", result);
+      let id = req.body.id;
+      // id = ObjectId(id);
+
+      const result = await Audio.find({
+        _id: mongoose.Types.ObjectId(id)
+      });
+      console.log("result", result, id);
       res.status(200).json(result);
     } catch (e) {
       console.log(e);
